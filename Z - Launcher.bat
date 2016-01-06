ea6683377146ff9da4c8683afc8afd0f9206bbe6
RandomUsername96 <koorweetzaah2@gmail.com>
2016-01-07 00:29:30 +0100

    fix

--- Z - Launcher.bat
+++ Z - Launcher.bat
@@ -1,5 +1,5 @@
 @start /b cmd /c java -jar "AngryMobsArena.jar" 
 timeout 1
-@start /b cmd /c "microsmeta\bin\Debug\Microsmeta.exe"
+@start /b cmd /c "microsmeta~\bin\Debug\Microsmeta.exe"
 timeout 1
 @start /b cmd /c "hackathon_binaries\yolo4.exe"
